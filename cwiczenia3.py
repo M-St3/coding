caed1b8a9f24a11661a0d78a6b1aa03dbe6463b9
M-St3 <mstepczyk88@wp.pl>
2024-10-24 08:44:55 +0200

    Add files via upload

---
+++ cwiczenia3.py
@@ -0,0 +1,48 @@
+# zad 1 a)
+#
+# suma= sum(i for i in range(0, 101, 2))
+# print(suma)
+#
+#zad.1 c)
+# suma= sum(i**2 for i in range(0,101))
+# print(suma)
+#
+#Zad.1 d)
+# suma= sum(2**i for i in range(0,63))
+# print(suma)
+#
+#Zad.1 e)
+# a=7
+# b=9
+# if a > b:
+#     suma= 0
+# else:
+#     suma= sum(i for i in range(a, b+1) if i%2 !=0)
+# print(suma)
+#
+#zad.1 f)
+# a= int(input("Wprowadź dowolną liczbę całkowitą:"))
+# if a > 0:
+#     for i in range(1,a+1):
+#         print("*"*i)
+# else:
+#     print("Podana liczba nie jest dodatnia!!!")
+#
+#zad.1 g)
+# n=4
+# suma=0.0
+# liczba= float(input("wprowadź wartość:"))
+# for i in range (n):
+#     suma+= liczba
+#     liczba = liczba + 1
+# print(suma)
+#
+#zad.1 h)
+# n = int(input("pobierz wartość:"))
+# if n > 0:
+#     x=0
+#     for i in range(n):
+#         liczba= float(input("pobierz liczbę z klawiatury"))
+#         if liczba%3 ==0 and liczba%5 !=0:
+#             x+=1
+# print(x)
